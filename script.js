f3e0c522a81c8610a68bdbdefb88fe95cb01d5c5
ahallam <alexhallam75@gmail.com>
2020-07-24 17:25:54 -0400

    Loader working

--- script.js
+++ script.js
@@ -3,15 +3,29 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+
+function showLoadingSpinner() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+function removeLoadingSpinnger() {
+  if (!loader.hidden) {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+  }
+}
 
 // Get Quote from API
-// async function getQuote() {
+async function getQuote() {
+  showLoadingSpinner();
   const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
   const apiUrl =
     'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
   try {
     const response = await fetch(proxyUrl + apiUrl);
-    const data = await response.text();
+    const data = await response.json();
     console.log(data);
     // If author is blank, add 'Unknown'
     if (data.quoteAuthor === '') {
@@ -26,9 +40,11 @@ const newQuoteBtn = document.getElementById('new-quote');
       quoteText.classList.remove('long-quote');
     }
     quoteText.innerText = data.quoteText;
+    // Stop Loading, Show Quote
+    removeLoadingSpinnger();
   } catch (error) {
-    getQuote();
     console.log('Whoops, no quote', error);
+    getQuote();
   }
 }
 
